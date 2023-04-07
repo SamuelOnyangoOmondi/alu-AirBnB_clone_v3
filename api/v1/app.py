52971656f9b77480e7a68acca1ef5192dfe969cc
SamuelOnyangoOmondi <101511277+SamuelOnyangoOmondi@users.noreply.github.com>
2023-04-07 16:08:19 +0300

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@
 import os
 import sys
 from models import storage
-from api.views import app_views
+from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
 from flask_cors import CORS
 
